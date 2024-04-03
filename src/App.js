19da26d5e7248e315d03233479963261ac880c81
fhaj2s <firas.haj@smail.inf.h-brs.de>
2024-04-03 21:45:08 +0200

    changes commited

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklistKey")) || []); // hole ein exisiterende Liste aus einem Localstorage oder erstelle eine neue Liste
+  const [tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklistKey")) || []); // hole eine exisiterende Liste aus einem Localstorage oder erstelle eine neue Liste
   const [clickexisted_task, setExistedTask] = useState({}); // ein leeres Objekt, repräsentiert den Zustand eines geklickten Tasks (hierfür wird ein ganzen task_objekt von showtask für das Editieren zugewiesen werden)
 
   useEffect( () => {
